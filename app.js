6e079fbae3b9e286055fca8601e10ec86ac94932
Meiguro <meiguro@meiguro.com>
2013-12-15 15:17:28 -0800

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ simply.setText({
 simply.on('singleClick', function(e) {
   if (e.button == 'select') {
     simply.setText({ body: 'awesome' });
+  } else {
+    simply.setText({ subtitle: '(^_^)' });
   }
 });
 
