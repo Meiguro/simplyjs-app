2ffd1fa518ebbb608d13ded42327b54626c83a53
Meiguro <meiguro@meiguro.com>
2013-12-15 15:47:35 -0800

    Update app.js

--- app.js
+++ app.js
@@ -6,19 +6,35 @@ simply.setText({
 var catText = 'Feline';
 
 var fruitList = [
-  'apple',
-  'orange',
-  'mango',
-  'kiwi',
+  'apple', // 0
+  'orange', // 1
+  'mango', // 2
+  'kiwi', // 3
 ];
+
+// Math.random() -> 0.5
+// Math.random() * fruitList.length -> 2
+// Math.floor(2.232) = 2
+
+var fruitIndex = 0;
+
+var getRandomIndex = function() {
+  return Math.floor(Math.random() * fruitList.length);
+};
+
+var updateFruit = function() {
+    simply.setText({ body: fruitList[fruitIndex] });
+};
 
 simply.on('singleClick', function(e) {
   if (e.button === 'select') {
-    simply.setText({ body: fruitList[2] });
+    // Do nothing
   } else if (e.button === 'up') {
-    simply.setText({ subtitle: catText });
-  } else {
-    simply.setText({ title: 'Goodbye' });
+    if (--fruitIndex < 0) { fruitIndex = fruitList.length - 1; }
+    updateFruit();
+  } else if (e.button === 'down') {
+    if (++fruitIndex >= fruitList.length) { fruitIndex = 0; }
+    updateFruit();
   }
 });
 
