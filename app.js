0411161ad3b0f7aa8ebcc0848dad802f10bc94a6
Meiguro <meiguro@meiguro.com>
2013-12-15 15:24:28 -0800

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ simply.setText({
   subtitle: 'Cruel',
 });
 
+// a-z 0-9 _
+
+var cat_text = 'Feline';
+
 simply.on('singleClick', function(e) {
   if (e.button === 'select') {
     simply.setText({ body: 'awesome' });
   } else if (e.button === 'up') {
-    simply.setText({ subtitle: '(^_^)' });
+    simply.setText({ subtitle: cat_text });
   } else {
     simply.setText({ title: 'Goodbye' });
   }
