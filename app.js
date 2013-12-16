8f6033a6fc6d5f4c213fa7333da7f533444a262c
Meiguro <meiguro@meiguro.com>
2013-12-15 16:05:42 -0800

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk';
 
 var requestWeather = function() {
   ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    simply.setText({ subtitle: data.main.temp });
+    simply.setText({ subtitle: data.main.temp.toString() });
   });
 };
 
