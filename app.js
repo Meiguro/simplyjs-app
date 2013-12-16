a4d174688a6cc55c29ed5ac6bca115848a91f4f2
Meiguro <meiguro@meiguro.com>
2013-12-15 16:18:11 -0800

    Update app.js

--- app.js
+++ app.js
@@ -11,18 +11,35 @@ var getRandomIndex = function() {
 };
 */
 
-var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk';
+var locations = [
+  'London,uk',
+  'San+Jose,us',
+  'Palo+Alto,us',
+];
+
+var locationIndex = 0;
+
+var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
 
 var requestWeather = function() {
-  ajax({ url: weatherUrl, type: 'json' }, function(data) {
+  var url = weatherUrl + locations[locationIndex];
+  var success = function(data) {
     simply.setText({ subtitle: data.main.temp.toString() });
-  });
+  };
+  var failure = function(data, status) {
+    simply.setText({ subtitle: ':( ' + status }, true);
+  };
+  ajax({ url: url, type: 'json' }, success, failure);
+  simply.setText({ subtitle: 'Loading...' });
 };
-
+  
 simply.on('singleClick', function(e) {
-  if (e.button == 'select') {
+  if (e.button == 'down') {
+    if (++locationIndex >= locations.length) { locationIndex = 0; }
     requestWeather();
   }
 });
 
 simply.begin();
+
+requestWeather();
